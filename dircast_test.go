ff2ebe03e6bb5bc828c068d01ce9934284a9f21e
Frode Aannevik <frode.aa@gmail.com>
2015-01-15 20:18:37 +0100

    formatYear: test support yyyyddMM format (fallback)

--- dircast_test.go
+++ dircast_test.go
@@ -8,6 +8,7 @@ func TestFormatYear(t *testing.T) {
 	}{
 		{"2015", "Thu, 01 Jan 2015 00:00:00 +0000"},
 		{"20151102", "Mon, 02 Nov 2015 00:00:00 +0000"},
+		{"20152402", "Tue, 24 Feb 2015 00:00:00 +0000"},
 	}
 	for _, c := range cases {
 		got := formatYear(c.in)
