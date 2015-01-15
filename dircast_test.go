cfd1e5ad965065ddbd5c4d205d9a170b66ec5da4
Frode Aannevik <frode.aa@gmail.com>
2015-01-15 20:16:01 +0100

    formatYear: test year yyyy only input

--- dircast_test.go
+++ dircast_test.go
@@ -2,4 +2,16 @@ package main
 
 import "testing"
 
-
+func TestFormatYear(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"2015", "Thu, 01 Jan 2015 00:00:00 +0000"},
+	}
+	for _, c := range cases {
+		got := formatYear(c.in)
+		if got != c.want {
+			t.Errorf("formatYear(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
